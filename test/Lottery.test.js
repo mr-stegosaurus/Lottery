9a1a570beeed8c6df35e4b018ac3b0fb8fe22280
Justin Smith <smithjustin158@gmail.com>
2021-05-20 19:47:24 -0400

    updated tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -36,7 +36,7 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[0], players[0]);
         assert.equal(1, players.length)
     });
-    
+
     it('allows multiple accounts to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
@@ -71,5 +71,36 @@ describe('Lottery Contract', () => {
         } catch (err) {
             assert(err);
         }
+    });
+
+    it('only manager can call pick winner', async () => {
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('sends money to the winner and resets the player array', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]
+        });
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+        console.log(finalBalance - initialBalance);
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
     });
 });
